aee5eb4f9818c517bfd9b55a0aa090e0660e00d5
DZiber <dziber11@gmail.com>
2018-03-14 20:29:22 +0300

    Add group modification test

---
+++ src/test/java/address_book/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package address_book.tests;
+
+import address_book.model.GroupData;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ZeBr on 14.03.2018.
+ */
+public class GroupModificationTests extends TestBase {
+
+    @Test
+    public void testGroupModification () {
+        app.getNavigationHelper().gotoGroupPage();
+        app.getGroupHelper().selectGroup();
+        app.getGroupHelper().initGroupModification();
+        app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
+        app.getGroupHelper().submitGroupModification();
+        app.getGroupHelper().returnToGroupPage();
+    }
+}
